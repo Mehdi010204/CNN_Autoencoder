f927929c77b0e50255f72bcf45dbb03d3e7ab727
Mehdi010204 <mehdiberrada124@gmail.com>
2025-01-08 00:56:46 +0100

    changes

--- train_test.py
+++ train_test.py
@@ -3,6 +3,7 @@ from tensorflow.keras.callbacks import EarlyStopping
 from Models.autoencoder import Autoencoder
 from Utils.data_preprocessing import load_and_preprocess_data
 from Utils.custom_loss import hybrid_loss
+from Utils import metrics, visualization
 
 # Charger les données
 train_data, test_data = load_and_preprocess_data("data.csv")
@@ -24,3 +25,17 @@ autoencoder.fit(
     validation_data=(test_data, test_data),
     callbacks=[early_stopping]
 )
+
+# Compiling the model
+decoded_imgs = Autoencoder.predict(test_data)
+
+# Visualiazing the reconstructed images
+visualization.visualize_reconstructions(test_data, decoded_imgs, num_images=5, random_selection=True)
+
+# PSNR
+psnr = metrics.calculate_psnr(test_data, decoded_imgs)
+print(f"PSNR: {psnr:.2f} dB")
+
+# Compression rate
+compression_rate = metrics.calculate_compression_rate(test_data, decoded_imgs)
+print(f"Compression rate: {compression_rate:.2f}")
